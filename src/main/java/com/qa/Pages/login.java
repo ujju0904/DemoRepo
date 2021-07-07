63a1b321d9dd8fc32dd34679bf8027c0ac804b6c
Ujjval Patel <ujjvalpatel@Ujjvals-MacBook-Pro.local>
2021-07-07 19:20:22 -0300

    add serch page code

--- src/main/java/com/qa/Pages/login.java
+++ src/main/java/com/qa/Pages/login.java
@@ -2,4 +2,19 @@ package com.qa.Pages;
 
 public class login {
 
+	
+	
+	public void loginpage () {
+		System.out.println("login page");
+	
+	}
+	
+	
+	
+	
+	public void loginset() {
+		System.out.println("loginset");
+	}
 }
+
+
